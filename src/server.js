605608a1100441e85c925a87b259e0f5963f760d
BuildTools <unconfigured@null.spigotmc.org>
2019-08-30 11:00:27 -0300

    Adicionado porta do heroku

--- src/server.js
+++ src/server.js
@@ -4,14 +4,24 @@ const cors = require('cors');
 
 const routes = require('./routes/routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+io.on('connection', socket => {
+  socket.on('hello', message => {
+    console.log(message);
+  })
+  console.log("Nova conexão ", socket.id);
+});
 
 mongoose.connect('mongodb+srv://rafahsena:senha@cluster0-f8oqu.mongodb.net/test?retryWrites=true&w=majority', { 
   useNewUrlParser: true 
 });
 
-server.use(express.json());
-server.use(cors());
-server.use(routes);
+app.use(express.json());
+app.use(cors());
+app.use(routes);
 
-server.listen(3000);
+server.listen(process.env.PORT | 3000);
